240ea954ee1a70aba621f5960d441a8f68ad08d9
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-10-06 23:44:00 +0530

    [Prasoon] Added JS uc-1

---
+++ Sequence_Practice_Problem/Sequence-practice-problem.js
@@ -0,0 +1,5 @@
+//Usecase 1 - generate a random number
+console.log("***************************************");
+var randomValue =Math.floor(Math.random()*10);
+console.log("Random number between 0 and 10 is :"+randomValue);
+console.log("***************************************")
