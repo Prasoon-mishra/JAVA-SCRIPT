96b5221b141defb0487dc51ba41ccf313052dbf7
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-10-06 23:47:32 +0530

    [Prasoon] added UC-2

--- Sequence_Practice_Problem/Sequence-practice-problem.js
+++ Sequence_Practice_Problem/Sequence-practice-problem.js
@@ -3,3 +3,7 @@ console.log("***************************************");
 var randomValue =Math.floor(Math.random()*10);
 console.log("Random number between 0 and 10 is :"+randomValue);
 console.log("***************************************")
+//Usecase 2 - Generate a random number for dice
+var diceValue =Math.floor(Math.random()*6)+1;
+console.log("Random number between 1 and 6 for dice is :"+diceValue);
+console.log("***************************************");
