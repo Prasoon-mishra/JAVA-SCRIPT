e96156afcc3e652edc99ce26e4a086d0620807dd
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-10-06 23:58:43 +0530

    [Prasoon] added UC-4

--- Sequence_Practice_Problem/Sequence-practice-problem.js
+++ Sequence_Practice_Problem/Sequence-practice-problem.js
@@ -13,3 +13,14 @@ console.log("Random second number between 1 and 6 for dice is :"+diceValue2);
 var result = diceValue+diceValue2;
 console.log("The added dice numbers is :"+result);
 console.log("***************************************");
+//Usecase 4 - Find 5 random 2 digit value and their sum with average
+var sum = 0;
+for(var i = 1;i<=5;i++)
+{
+    let number = Math.floor(Math.random()*(100-10 +1))+10
+    console.log(i +" number is : "+number);
+    sum+=number;
+}   
+console.log("The sum is : "+sum);
+console.log("The average is :" +sum/5);
+console.log("***************************************");
